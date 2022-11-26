9c6c8c15eb87b424b58a9bb2189e28cd90eaa1fb
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-26 19:13:53 +0700

    Разница между min и max числами массива

---
+++ Задача3/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+
+FillArrayRandomNumbers(numbers, 1, 99);
+WriteArray(numbers);
+
+double max = numbers[0];
+double min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (max < numbers[i])
+    {
+        max = numbers[i];
+    }
+    if (min > numbers[i])
+    {
+        min = numbers[i];
+    }
+}
+
+double dif = max-min;
+
+Console.WriteLine($"{dif}");
+
+void FillArrayRandomNumbers(double[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
